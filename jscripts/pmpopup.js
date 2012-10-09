41710eb763d9dcf9e843a0ea8b6179948be4ff12
Steeep <hasaezs@gmail.com>
2012-10-09 14:37:25 -0400

    Edit pmpopup.js

--- jscripts/pmpopup.js
+++ jscripts/pmpopup.js
@@ -6,15 +6,5 @@ var mpPopup = {
 	refresh: function()
 	{
 		setTimeout("mpPopup.refresh();", mpPopup.refreshInterval * 1000);
-		if (typeof Ajax == 'object')
-		{
-			new Ajax.Request('xmlhttp.php?action=checku_mps&uid=' + mpPopup.uID,
-			{
-				method: 'get',
-				onComplete: function(request) {
-					ShoutBox.shoutsLoaded(request);
-				}
-			});
-		}
 	}
 };
